760fb7ee7721eb70271b7bc64e59d0095e9b90a5
fernandolopezjava <67985041+fernandolopezjava@users.noreply.github.com>
2020-08-25 19:47:43 -0300

    Add files via upload
    buffer, fpurge//fflush, contadores

---
+++ ejercicio 2.c
@@ -0,0 +1,68 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+// una linea
+/*
+
+una linea
+otra linea
+
+*/
+
+/*
+
+js->interpretado
+c ->compilado 
+
+
+Entradas y salidas
+
+js->prompt / id.value
+c -> scanf
+
+js -> alert / document.write / console.log / id.value
+c -> printf
+
+
+Tipos de datos:
+
+int -> 4, -8, 1000
+float -> Todos los reales: -9.25, 100.1 , 7/9 , 7, 0, -8
+char -> UN SOLO CARACTER: 'i', '9', '@', '-'
+void -> sin tipo
+double -> flotantes con mayor precision
+-------------------
+creados por el programador
+-------------------
+No existen:
+Booleanos
+Strings
+
+Mascaras de entrada:
+int -> "%d" // "%i"
+float -> "%f"
+char -> "%c"
+
+*/
+
+int main()//Punto de entrada Entry point
+{
+    int numero;
+    float otroNumero;
+    
+    numero = 7;
+    otroNumero = 32.954;
+    
+    
+    printf("El numero es: %d y el otro numero es: %f", numero, otroNumero);
+    return 0;
+}
+
